31f42e5f81cd5a2d79a6d8d0de7c8bce12c2bf66
roshansingh2719@gmail.com <roshansingh2719@gmail.com>
2023-11-26 13:56:19 +0530

    final

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return (
+    <Image
+      height={130}
+      width={230}
+      alt="logo"
+      src="/project.png"
+    />
+  )
+}
+
+export const Logo1 = () => {
+  return (
+    <Image
+      height={30}
+      width={330}
+      alt="logo"
+      src="/projec.png"
+    />
+  )
+}
+
+export const Home = () => {
+  return (
+    <Image
+      height={430}
+      width={530}
+      alt="logo"
+      src="/home.jpg"
+    />
+  )
+}
